0a24b923c34797b995b92be2f07208bc23708bd2
Zhukov Vitaliy <felex180590@gmail.com>
2024-09-12 16:45:00 +0300

    adds mobile first

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,14 +1,31 @@
-import Swiper from "swiper";
-import "swiper/swiper-bundle.css";
+// import Swiper from 'swiper';
+// import 'swiper/swiper-bundle.css';
 
-const swiper = new Swiper(".swiper-container", {
-  direction: "horizontal",
-  loop: true,
+const swiper = new Swiper('.swiper', {
+  // direction: 'horizontal',
+  // loop: true,
   pagination: {
-    el: ".swiper-pagination",
+    el: '.swiper-pagination',
+    clickable: true,
   },
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
   },
+  // loop: true,
+  grabCursor: true,
+  // slidesPerView: 3,
+  // slidesPerView: auto,
+
+  //.swiper-slide {
+  // width:auto;
+  // }
+  // rbeakpoints: {
+  //   320: {
+  //     slidesPerView: 1,
+  //   },
+  //   600: {
+  //     slidesPerView: auto,
+  //   },
+  // },
 });
